7c4879b2811ea7c8daa964a121e420fd156e7727
APatton3472 <60272264+APatton3472@users.noreply.github.com>
2020-03-03 18:26:37 -0500

    Initial commit

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void play()
+    {
+        Application.LoadLevel(1);
+    }
+    public void MM()
+    {
+        Application.LoadLevel(0);
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void credits()
+    {
+        Application.LoadLevel(4);
+    }
+}
